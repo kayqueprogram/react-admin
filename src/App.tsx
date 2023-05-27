d732f48f6f5c6e9a3b43cc5e201c5055a81cdc9a
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-27 15:46:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import { Admin, Resource, ListGuesser, EditGuesser, ShowGuesser } from 'react-admin';
+import { dataProvider } from './dataProvider';
+import { authProvider } from './authProvider';
+import { UserList } from './Components/UserList';
+
+export const App = () => (
+    <Admin
+        dataProvider={dataProvider}
+		authProvider={authProvider}
+	>
+        <Resource name="users" list={UserList} />
+    </Admin>
+);
+
+    
