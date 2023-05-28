24262d611602ac5bcbbce99b02cad603622a6d9f
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-27 22:36:21 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 // admin.tsx
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
+import { authProvider } from './authProvider';
 import dataProvider from './firebase/firebaseDataProvider';
 import  CreateRandomPost  from './Components/PostList';
 
 export const App: React.FC = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} authProvider={authProvider}>
     <Resource name="posts" list={CreateRandomPost} />
   </Admin>
 );
