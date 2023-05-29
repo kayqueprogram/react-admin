90f47ebc882c07c93ce09467b2a767d85fdf0ebb
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-29 18:28:51 -0300

    teste

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,23 +1,19 @@
 import firebase from "firebase/app";
-import "firebase/auth";
 import "firebase/firestore";
-import { FirebaseAuthProvider, FirebaseDataProvider } from "react-admin-firebase";
+import { FirebaseDataProvider } from "react-admin-firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB376x4XGMwBAJ-n5Ov4It4VRShv6-MCBE",
-    authDomain: "omardonatobassani-c3633.firebaseapp.com",
-    projectId: "omardonatobassani-c3633",
-    storageBucket: "omardonatobassani-c3633.appspot.com",
-    messagingSenderId: "679823851464",
-
-    appId: "1:679823851464:web:8ab1dadb7b48044aeb0afd",
-    measurementId: "G-8DNJHR1WQ6"
+  apiKey: "AIzaSyB376x4XGMwBAJ-n5Ov4It4VRShv6-MCBE",
+  authDomain: "omardonatobassani-c3633.firebaseapp.com",
+  projectId: "omardonatobassani-c3633",
+  storageBucket: "omardonatobassani-c3633.appspot.com",
+  messagingSenderId: "679823851464",
+  appId: "1:679823851464:web:8ab1dadb7b48044aeb0afd",
+  measurementId: "G-8DNJHR1WQ6"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const dataProvider = FirebaseDataProvider(firebaseConfig);
 
-export { dataProvider };
-
-
+export default dataProvider;
