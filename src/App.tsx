4eb400bc4495817957213e88085d75e69c1f432d
Kayque de Jesus <kayquejesusdn@gmail.com>
2023-05-30 16:37:21 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-// admin.tsx
 import React from 'react';
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser, EditGuesser, Create, SimpleForm, TextInput } from 'react-admin';
 import { authProvider } from './authProvider';
-import  dataProvider  from './firebase/firebaseConfig';
-import CreateRandomPost from './Components/PostList';
+import dataProvider from './firebase/firebaseConfig';
+import CreateRandomPost from './Components/CreateRandomPost';
 
 const App = () => (
-  <Admin dataProvider={dataProvider} authProvider={authProvider}>
-    <Resource name="posts" list={ListGuesser} />
+  <Admin dataProvider={dataProvider} authProvider={authProvider} >
+    <Resource name="posts" list={ListGuesser} edit={EditGuesser} create={PostCreate} />
+    <CreateRandomPost />
   </Admin>
 );
 
+const PostCreate: React.FC = (props) => (
+  <Create title="Criar um Post" {...props}>
+    <SimpleForm>
+      <TextInput source='title' label='Título' />
+      <TextInput multiline source='content' label='Conteúdo' />
+    </SimpleForm>
+  </Create>
+);
+
 export default App;
-
-
